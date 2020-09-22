520d8e0efb5e9f950e52f041951bf4413b50379d
victor atencio <victorj.atencio@gmail.com>
2020-09-22 09:07:49 -0400

    finished header

---
+++ src/Home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Banner from "../Banner/Banner";
+
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <Banner />
+    </div>
+  );
+}
+
+export default Home;
